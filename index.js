f4fb1b9cf9b3e28a0d0970f7dd1fc2adcfd439e0
Wesley LeMahieu <lateralis3k@gmail.com>
2022-07-02 11:45:49 -0700

    allow looping through child elements

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 /**
  * POST with a payload formatted as:
  * [{
- *    "url": <string>,
- *    "selector": <string>,
- *    "timeout": <number>,
- *    "eval": <string>,
- *    "key": <string>
+ *    "url": <string>,        (what url to crawl)
+ *    "selector": <string>,   (what to select)
+ *    "timeout": <number>,    (selector timeout)
+ *    "type": <string>,       (single or multiple items)
+ *    "eval": <string>,       (function to run)
+ *    "attribute": <string>,  (attribute to extract)
+ *    "key": <string>         (response object value property)
+ *    "element": []           (optional array of child elements)
  * }]
+ * 
  */
 const functions = require('@google-cloud/functions-framework');
 const { finalize, initialize, visitUrl } = require('./puppeteer');
 
 const findSelector = async (page, element, values) => {
   try {
+    // Wait for selector...
     console.log(`Waiting for '${element.selector}' selector...`);
     await page.waitForSelector(element.selector, { visible: true, timeout: element.timeout });
     console.log('Selector found!');
-            
+           
+    // Use specified function to extract values...
     let value;
 
     if (element.type === 'multiple') {
@@ -43,10 +49,27 @@ const findSelector = async (page, element, values) => {
       }
     }
 
+    // set value
     values[element.key] = value;
+
+    // loop through any child elements
+    if (element.element) {
+      await crawElements(page, element.elements, values);
+    }
+
+    return Promise.resolve();
 
   } catch (e) {
     console.log(`Selector not found.`, e);
+  }
+};
+
+// iterate through all elements
+const crawElements = async (page, elements, values) => {
+  for (const key in elements) {
+    const element = elements[key];
+    console.log('Crawling element through proxy...', element);
+    await findSelector(page, element, values);
   }
 };
 
@@ -55,22 +78,12 @@ functions.http('run', async (req, res) => {
     const elements = JSON.parse(req.body.elements);
     const url = req.body.url;
     const values = {}; // extracted values
-
-    // initialize puppeteer
     const headless = true;
     const [browser, page] = await initialize(headless);
     await visitUrl(url, page);
-
-    // iterate through requested elements
-    for (const key in elements) {
-      const element = elements[key];
-      console.log('Crawling element through proxy...', element);
-      await findSelector(page, element, values);
-    }
-
+    await crawElements(page, elements, values);
     await finalize(browser);
     res.send(values);
-
   } else {
     res.send(`Example Web Crawler Extracter Google Cloud Function`);
   }
